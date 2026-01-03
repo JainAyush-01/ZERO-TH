6156c087339d7b21032ca57d23816b691eed91eb
JainAyush-01 <ayushj9027@gmail.com>
2026-01-03 20:41:18 +0530

    Corrected package.json

--- Day9_cpy/src/config/redis.js
+++ Day9_cpy/src/config/redis.js
@@ -5,7 +5,7 @@ const redisClient = redis.createClient({
     password: process.env.REDIS_PASS,
     socket: {
         host: process.env.REDIS_HOST,
-        port: 12691
+        port: process.env.REDIS_PORT
     }
 });
 
